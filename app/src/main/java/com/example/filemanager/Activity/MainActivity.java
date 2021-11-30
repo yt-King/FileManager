1878fff37d2c9e1f79bbb227ca2a32f909150e73
应涛 <yt1577396227@163.com>
2021-11-30 10:24:10 +0800

    删除移动文件并合并功能

--- app/src/main/java/com/example/filemanager/Activity/MainActivity.java
+++ app/src/main/java/com/example/filemanager/Activity/MainActivity.java
@@ -64,73 +64,6 @@ public class MainActivity extends BaseActivity {
   }
 
   @Override
-  public void init() {
-    super.init();
-    if (!path.equals(GetFilesUtils.getInstance().getBasePath())) {
-      //添加默认的返回图标
-      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-      //设置返回键可用
-      getSupportActionBar().setHomeButtonEnabled(true);
-    }
-    setTitle(new File(path).getName());
-    getSupportActionBar().setSubtitle(path);
-    RecyclerView recyclerView = findViewById(R.id.file_container);
-
-    ItemTouchCallBack touchCallBack = new ItemTouchCallBack(adapter);
-    ItemTouchHelper itemTouchHelper = new ItemTouchHelper(touchCallBack);
-    itemTouchHelper.attachToRecyclerView(recyclerView);
-    //  拖拽文件的监听函数
-    recyclerView.setOnTouchListener((v, event) -> {
-      int type = event.getAction();
-      switch (type) {
-        case MotionEvent.ACTION_DOWN:
-        case MotionEvent.ACTION_MOVE:
-          break;
-        case MotionEvent.ACTION_UP:
-          Log.i(TAG, "recyclerView touch UP");
-          if (adapter.getFromPosition() != -1) {
-            adapter.notifyItemRemoved(adapter.getFromPosition());
-            FileView source = fileList.get(adapter.getFromPosition());
-            FileView target = fileList.get(adapter.getToPosition());
-            //  文件/文件夹 拖拽到文件夹，则将文件/文件夹移动到目标文件夹中
-            if (target.isFolder()) {
-              Log.d(TAG, "folder to folder");
-              try {
-                FileManagerUtils.Instance.moveToFolder(source.getFile(), target.getFile());
-                fileList.remove(source);
-                adapter.setItemCheckStates(adapter.getFromPosition(), false);
-                adapter.notifyOperationFinish();
-                Toast.makeText(this, "移动成功~", Toast.LENGTH_SHORT).show();
-              } catch (IOException e) {
-                e.printStackTrace();
-              }
-            } else if (!source.isFolder() && !target.isFolder()) {
-              //  文件拖拽到文件，则将两者合并为一个文件夹
-              String newFolderName = source.getFileName() + "和" + target.getFileName();
-              File newFolder = new File(FilenameUtils.concat(source.getFile().getParent(), newFolderName));
-              try {
-                FileManagerUtils.Instance.mergeIntoFolder(source.getFile(), target.getFile(), newFolder);
-                fileList.remove(source);
-                fileList.remove(target);
-                fileList.add(new FileView(newFolder));
-                adapter.setItemCheckStates(adapter.getFromPosition(), false);
-                adapter.notifyOperationFinish();
-                Toast.makeText(this, "移动成功~", Toast.LENGTH_SHORT).show();
-              } catch (IOException e) {
-                e.printStackTrace();
-              }
-            }
-            adapter.setFromPosition(-1);
-            return true;
-          }
-          break;
-
-      }
-      return false;
-    });
-  }
-
-  @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     //  进入搜索页面
     if (item.getItemId() == R.id.search) {
@@ -191,7 +124,7 @@ public class MainActivity extends BaseActivity {
   //滑动事件
   private class GestureListener extends GestureDetector.SimpleOnGestureListener {
 
-    private final int SWIPE_MIN_DISTANCE = 150;
+    private final int SWIPE_MIN_DISTANCE = 400;
     private final int SWIPE_THRESHOLD_VELOCITY = 100;
 
     @Override
